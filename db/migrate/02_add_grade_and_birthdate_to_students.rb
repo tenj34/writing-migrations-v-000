8a6af008a8947d15e4dff38a4cb8226e73e35477
Juan Tenezaca <tenj34@gmail.com>
2018-10-01 23:00:50 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    add_column :artists, :favorite_food, :string
+    change_column :students, :birthdate, :datetime
   end
 end
